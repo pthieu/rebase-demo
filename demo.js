fc0cd77467ea191d665cee9bc3d5b8ee9e323d98
Phong Thieu <pthieu@gmail.com>
2017-07-25 23:59:43 -0400

    Demo2: dev1 adds a line

--- demo.js
+++ demo.js
@@ -6,4 +6,5 @@ function main() {
   console.log('this is the master branch');
   console.log('pr adds an extra line, should be no conflict')
   console.log('dev2 adds this line')
+  console.log('dev1 adds this line xxx')
 }
